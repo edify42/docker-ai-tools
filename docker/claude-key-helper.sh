f6e569cfbfd94046539c7ac3b12a880d76c4a18e
ed kim <edify42@yahoo.com>
2025-10-21 10:48:43 +1100

    cleanup
    Signed-off-by: ed kim <edify42@yahoo.com>

--- docker/claude-key-helper.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# echo a short-lived key from your secret store, or a static key:
-printf "%s" "$ANTHROPIC_API_KEY"
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -25,8 +25,48 @@ mkdir -p .claude
 
 cat << EOF > .claude/settings.json
 {
-  "apiKeyHelper": "/home/node/scripts/claude-key-helper.sh",
   "theme": "dark"
+}
+EOF
+
+cat << EOF > .claude.json
+{
+  "numStartups": 2,
+  "hasCompletedProjectOnboarding": true,
+  "customApiKeyResponses": {
+    "approved": [
+      "${ANTHROPIC_API_KEY: -20}"
+    ],
+    "rejected": []
+  },
+  "firstStartTime": "2025-10-20T23:22:56.156Z",
+  "userID": "549478dba14de2ed8dc1de90d395dab304b58152dced3acda723d427114cbb6f",
+  "sonnet45MigrationComplete": true,
+  "projects": {
+    "/workdir": {
+      "allowedTools": [],
+      "history": [
+        {
+          "display": "/exit",
+          "pastedContents": {}
+        }
+      ],
+      "mcpContextUris": [],
+      "mcpServers": {},
+      "enabledMcpjsonServers": [],
+      "disabledMcpjsonServers": [],
+      "hasTrustDialogAccepted": true,
+      "ignorePatterns": [],
+      "projectOnboardingSeenCount": 1,
+      "hasClaudeMdExternalIncludesApproved": false,
+      "hasClaudeMdExternalIncludesWarningShown": false,
+      "exampleFiles": []
+    }
+  },
+  "hasCompletedOnboarding": true,
+  "lastOnboardingVersion": "2.0.24",
+  "hasOpusPlanDefault": false,
+  "lastReleaseNotesSeen": "2.0.24"
 }
 EOF
 
